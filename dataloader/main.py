d08231f26eaf605d1cf11d3a8782941da9522983
Tim <bleimehl@gmail.com>
2020-05-18 12:43:05 +0200

    Update main.py

--- dataloader/main.py
+++ dataloader/main.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     SCRIPT_DIR = os.path.dirname(
         os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__)))
     )
-    SCRIPT_DIR = os.path.join(SCRIPT_DIR, "..")
-    sys.path.append(os.path.normpath(SCRIPT_DIR))
+    PARENT_DIR = os.path.join(SCRIPT_DIR, "..")
+    sys.path.append(os.path.normpath(PARENT_DIR))
 
 dataset_path = os.path.join(SCRIPT_DIR,"../dataset")
 
