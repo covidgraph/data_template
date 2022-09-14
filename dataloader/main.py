532de9a975499edc315c17e13a249568e840c1a6
motey <bleimehl@gmail.com>
2022-09-14 09:27:05 +0200

    wip

--- dataloader/main.py
+++ dataloader/main.py
@@ -3,29 +3,23 @@ import sys
 import json
 import py2neo
 
-if __name__ == "__main__":
-    SCRIPT_DIR = os.path.dirname(
-        os.path.realpath(os.path.join(
-            os.getcwd(), os.path.expanduser(__file__)))
-    )
-    PARENT_DIR = os.path.join(SCRIPT_DIR, "..")
-    sys.path.append(os.path.normpath(PARENT_DIR))
 
-dataset_path = os.path.join(PARENT_DIR, "dataset")
-
-download_dummy_function(dataset_path)
-
-
-neo4j_config_str = os.getenv('NEO4J', {"host": "localhost"})
+# we load the neo4j database connection details from the environment variable `NEO4J`
+neo4j_config_str = os.getenv("NEO4J", {"host": "localhost"})
 neo4j_config_dict = json.load(neo4j_config_str)
-
-ENV = os.getenv('ENV', 'prod')
-
+# Connect to the neo4j database
 graph = py2neo.Graph(**neo4j_config_dict)
 
-dummy_data = data_parsing_dummy_function(dataset_path)
 
+ENV = os.getenv("ENV", "prod")
+
+
+# your dataloader logic
+dataset_url = "https://data.org/mydata.txt"
+download_dummy_function(dataset_url)
+dummy_data = data_parsing_dummy_function(dataset_path)
 for dd in dummy_data:
     # just an bad example :) dont do it like this
     graph.run(
-        "CREATE (n:Person { name: '"+dd.name+"', title: '"+dd.title+"' })")
+        "CREATE (n:Person { name: '" + dd.name + "', title: '" + dd.title + "' })"
+    )
